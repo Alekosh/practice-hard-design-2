648936fdd080a189a6dfbe59acf6edc76d66c4eb
Alekosh <aakhosh00@gmail.com>
2023-10-28 03:27:08 +0500

    created hard section

--- js/main.js
+++ js/main.js
@@ -29,3 +29,52 @@ window.addEventListener('scroll', () => {
 
   lastScroll = scrollPosition();
 })
+// =============================
+// const fullHeight = document.body.scrollHeight;
+// const innerHeight = window.innerHeight;
+// const dashed = document.querySelector('.dashed');
+// const scrollBar = document.querySelector('.releases__scroll');
+
+// window.addEventListener('scroll', () => {
+//   console.log(fullHeight);
+//   console.log(innerHeight);
+
+//   fillScrollBar()
+// })
+
+// function fillScrollBar() {
+//   // if (fullHeight >= 6122) { //5666
+//   dashed.style.height = (scrollY * 100 / (fullHeight - innerHeight)) + '%';
+//   // dashed.style.height = (scrollY * 100 / (fullHeight / 50)) + '%';
+//   // dashed.style.height = (scrollY * 100 / (fullHeight / 100) / innerHeight + '%');
+//   // dashed.style.height = (scrollY * 100 / (fullHeight / 100)) + '%';
+//   // dashed.style.height = (scrollY * 100 / (fullHeight + '%'));
+//   // }
+//   // dashed.style.height = (window.scrollY / 100 / (scrollBar * 1) + '%')
+// }
+// =====================
+// const all = document.documentElement;
+
+// let scrollHeight = Math.max(
+//   document.body.scrollHeight, document.documentElement.scrollHeight,
+//   document.body.offsetHeight, document.documentElement.offsetHeight,
+//   document.body.clientHeight, document.documentElement.clientHeight
+// );
+
+// console.log(scrollHeight);
+
+// const fullHeight = document.body.scrollHeight; // 6246 / 100 = 62,46
+// const scrollBar = document.querySelector('.releases__scroll');
+// const dashed = document.querySelector('.dashed');
+// console.log(scrollBar.scrollHeight); // 580 / 100 = 5,8
+// // const innerHeight = window.innerHeight;
+// // const dashed = document.querySelector('.dashed');
+// //5086
+// window.addEventListener('scroll', () => {
+//   fillScrollBar()
+// })
+
+// function fillScrollBar() {
+//   dashed.style.height = ((fullHeight / 100) * (scrollBar / 100) + '%');
+//   // dashed.style.height = (scrollY * 100 / (fullHeight - innerHeight)) + '%';
+// }
