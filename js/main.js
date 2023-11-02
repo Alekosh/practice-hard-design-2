a38fca914463ca6f55235f9683e594cf35cf5c7f
Alekosh <aakhosh00@gmail.com>
2023-11-02 06:21:52 +0500

    added more section

--- js/main.js
+++ js/main.js
@@ -78,3 +78,24 @@ window.addEventListener('scroll', () => {
 //   dashed.style.height = ((fullHeight / 100) * (scrollBar / 100) + '%');
 //   // dashed.style.height = (scrollY * 100 / (fullHeight - innerHeight)) + '%';
 // }
+// =====
+
+const accordionBtns = document.querySelectorAll(".faq__head");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
